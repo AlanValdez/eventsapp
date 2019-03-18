0cabbfd545c19da06fd74fae58faaf0032986fd6
alanb.valdez <alanb.valdez@softtek.com>
2019-03-18 14:40:02 -0600

    Events app module and component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/events-list.component';
+import { EventThumbNailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+@ NgModule({
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbNailComponent,
+    NavBarComponent
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
