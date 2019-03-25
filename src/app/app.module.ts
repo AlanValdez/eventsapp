2655d74d765ded5ffc6fb161f45d2876da96bbce
alanb.valdez <alanb.valdez@softtek.com>
2019-03-25 10:52:11 -0600

    App init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import {
+  EventListComponent,
+  EventThumbNailComponent,
+  EventRouterActivator,
+  EventDetailsComponent,
+  CreateEventComponent
+} from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/events-list.component';
-import { EventThumbNailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
 
 @ NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventListComponent,
     EventThumbNailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
+  ],
+  providers: [EventRouterActivator,
+  {
+    provide: 'canDeactivateCreateEvent',
+    useValue: checkDirtyState
+  }
   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you want to proceed?');
+  }
+  return true;
+}
---
+++ src/app/routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import {
+  EventListComponent,
+  EventRouterActivator,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventListResolver
+} from './events/index';
+import { Error404Component } from './errors/404.component';
+
+export const appRoutes: Routes = [
+  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
+  { path: 'events', component: EventListComponent, resolve: {events: EventListResolver} },
+  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouterActivator] },
+  { path: '404', component: Error404Component },
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: './user/user.module#UserModule'}
+];
